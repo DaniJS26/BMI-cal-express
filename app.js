8a037f280855c8b8fb547dca2ea9a5d3e8eedd4c
Dani JS <levi@JS.local>
2023-08-29 00:15:24 +0630

    Latest BMI calculator

--- app.js
+++ app.js
@@ -8,6 +8,26 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
 });
 
+app.post("/", (request, response) => {
+    let weight = parseFloat(request.body.weight);
+    let height = parseFloat(request.body.height);
+    let BMI = 703 * (weight / (height ** 2));
+    if (BMI >= 40.0) {
+        response.send("<h1>Obese.</h1>")
+    }
+    else if (BMI <= 18.4) {
+        response.send("<h1>Underweight.</h1>");
+    }
+    else if (BMI >=18.5 && BMI <= 24.9) {
+        response.send("<h1>Normal.</h1>");
+    }
+    else if (BMI >=25.0 && BMI <=39.9) {
+        response.send("<h1>Overweight.</h1>")
+    }
+})
+
+
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
